2f32fb1f039f95051b65ba77c9a6a46ef6a02f84
Maximiliano Montaña <maxi.omr01@gmail.com>
2022-08-06 00:12:06 -0300

    Deploy the project

---
+++ src/helpers/url.js
@@ -0,0 +1,10 @@
+const DB = "https://stocktaking-db.herokuapp.com";
+
+const url = {
+  front: "http://localhost:3000",
+  backTools: `${DB}/api/tools`,
+  backUsers: `${DB}/api/users`,
+  backReports: `${DB}/api/reports`,
+};
+
+export default url;
